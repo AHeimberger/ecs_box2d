2ffad1b12a15c06d8909f8c8f0d51e32bdbf33df
Silvan Wegmann <swegmann@narf.ch>
2018-05-29 20:31:54 +0200

    Add a first simple ImGui

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -8,10 +8,19 @@ MainWindow::MainWindow() :
     glfwMakeContextCurrent(window.get());
     glfwSwapInterval(1); // Enable vsync
     gl3wInit();
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    //ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui_ImplGlfwGL3_Init(window.get(), true);
+
+    ImGui::StyleColorsDark();
 }
 
 void MainWindow::loop()
 {
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     while (!glfwWindowShouldClose(window.get()))
     {
         // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
@@ -19,6 +28,25 @@ void MainWindow::loop()
         // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
         // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         glfwPollEvents();
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        {
+            ImGui::Begin("Main Menu");
+
+            static float f = 0.0f;
+            static int counter = 0;
+            ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
 
         // Rendering
         int display_w, display_h;
@@ -26,6 +54,8 @@ void MainWindow::loop()
         glViewport(0, 0, display_w, display_h);
         glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window.get());
     }
 }
