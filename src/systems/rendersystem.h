f347abc99093404c01c6dd134c654618e20af7bb
Silvan Wegmann <swegmann@narf.ch>
2018-06-14 22:15:10 +0200

    Cleanup source formatting

--- src/systems/rendersystem.h
+++ src/systems/rendersystem.h
@@ -7,14 +7,14 @@
 class RenderSystem : public entityx::System<RenderSystem>
 {
 public:
-   explicit RenderSystem(sf::RenderTarget& target);
+    explicit RenderSystem(sf::RenderTarget& target);
 
-   void update(entityx::EntityManager &entities,
-               entityx::EventManager &events,
-               double dt);
+    void update(entityx::EntityManager &entities,
+                entityx::EventManager &events,
+                double dt);
 
 private:
-   sf::RenderTarget& target;
+    sf::RenderTarget& target;
 };
 
 #endif // RENDERSYSTEM_H
