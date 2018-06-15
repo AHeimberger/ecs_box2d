f6115521e92283ec5906daa6517eb883d56ae285
Silvan Wegmann <silvan.wegmann@bbv.ch>
2018-06-15 09:25:19 +0200

    Fix case for included header

--- src/sfmldebugdraw.cpp
+++ src/sfmldebugdraw.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "SFMLDebugDraw.h"
+#include "sfmldebugdraw.h"
 
 SFMLDebugDraw::SFMLDebugDraw(sf::RenderTarget &window) : m_window(&window) {}
 
